d01528978a28f1c2f29783a3fdc6d8f1fbf8a96a
Samowusu <samuelowusu958@gmail.com>
2021-11-08 17:58:35 +0000

    smooth responsiveness added

---
+++ Frontend_Challenge/src/assets/store.js
@@ -0,0 +1,41 @@
+import cart from './images/icon-cart.svg';
+import logo from './images/logo.svg';
+import avt from './images/image-avatar.png';
+import prod1 from './images/image-product-1.jpg';
+import prod2 from './images/image-product-2.jpg';
+import prod3 from './images/image-product-3.jpg';
+import prod4 from './images/image-product-4.jpg';
+import tn1 from './images/image-product-1-thumbnail.jpg';
+import tn2 from './images/image-product-2-thumbnail.jpg';
+import tn3 from './images/image-product-3-thumbnail.jpg';
+import tn4 from './images/image-product-4-thumbnail.jpg';
+import plus from './images/icon-plus.svg';
+import minus from './images/icon-minus.svg';
+import menu from './images/icon-menu.svg';
+
+
+
+
+export const imagesStore = { images : {
+   
+    imageProductOne : prod1,
+    imageProductTwo : prod2,
+    imageProductThree : prod3,
+    imageProductFour : prod4
+}};
+
+export const thumbnailsStore = { thumbnails : {
+    thumbnailOne : tn1,
+    thumbnailTwo : tn2,
+    thumbnailThree : tn3,
+    thumbnailFour : tn4,
+}}
+
+export const extrasStore = { extras : {
+    cartIcon : cart,
+    avatar : avt,
+    sneakerLogo : logo,
+    addButton : plus,
+    subtractButton : minus,
+    toggleMenu : menu
+}}
---
+++ Frontend_Challenge/src/components/header.js
@@ -0,0 +1,33 @@
+import classes from './header.module.css';
+import { extrasStore } from '../assets/store';
+
+
+const Header = (props) => {
+    return (
+      <div className={classes.headerContainer}>
+        <header className={classes.header}>
+          <nav className={classes.nav}>
+          <img 
+            src={extrasStore.extras.toggleMenu} 
+            alt=''
+            className={classes.menu}
+          />
+          <img src={extrasStore.extras.sneakerLogo} alt='sneaker logo'/>  
+          <ul>
+            <li>Collections</li>
+            <li>Men</li>   
+            <li>Women</li>   
+            <li>About</li>   
+            <li>Contact</li>   
+          </ul>
+          </nav>
+          <section className={classes.profile}>
+          <img src={extrasStore.extras.cartIcon} alt='a cart'/>
+          <img className={classes.avatar} src={extrasStore.extras.avatar} alt='profile'/>
+          </section>
+        </header>
+        </div>
+    )
+};
+
+export default Header;

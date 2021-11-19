9d1444f3b5867e34da9adc4e4a37bce9636a02d6
Samuel Baah Owusu <samuelowusu958@gmail.com>
2021-11-19 10:29:24 -0100

    image functionality added

--- Frontend_Challenge/src/components/productImages.js
+++ Frontend_Challenge/src/components/productImages.js
@@ -6,15 +6,49 @@ import {  thumbnailsStore, pictures } from '../assets/store';
 const ProductImages = props => {
     const [displayImage , setDisplayImage] = useState(`${pictures[0]}`);
 
+    let image_tracker = 'first_pic';
     const changeDisplayImageHandler = () => {
-        let i=0;
 
-        if( i < pictures.length - 1){
-            i++;
-        } else {
-            i = 0;
+        switch (image_tracker) {
+            case 'first_pic': 
+             image_tracker = 'second_pic';
+             setDisplayImage(`${pictures[1]}`);
+
+             break;
+
+            case 'second_pic' :
+                image_tracker = 'third_pic';
+                setDisplayImage(`${pictures[2]}`);
+
+                break;
+
+            case 'third_pic':
+                image_tracker = 'fourth_pic';
+                setDisplayImage(`${pictures[3]}`);
+
+                break;
+
+            default:
+            setDisplayImage(`${pictures[0]}`);
+                
+             
         }
-        setDisplayImage(`${pictures[i]}`);
+        // if (image_tracker === 'first pic'){
+        //     image_tracker = 'second pic';
+        //     setDisplayImage(`${pictures[1]}`);
+
+        // } else if (image_tracker === 'second pic') {
+        //     image_tracker = 'third pic';
+        //     setDisplayImage(`${pictures[2]}`);
+
+        // } else if (image_tracker === 'third pic') {
+        //     image_tracker = 'fourth pic';
+        //     setDisplayImage(`${pictures[3]}`);
+
+        // }  else {
+        //     setDisplayImage(`${pictures[0]}`)
+        // }
+       
     };
 
 
